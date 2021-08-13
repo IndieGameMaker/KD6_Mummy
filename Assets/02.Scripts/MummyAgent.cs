683f2f8e18257f1df079a2680d98b1709734d479
Lee JaeHyun <myleje@gmail.com>
2021-08-13 14:26:17 +0900

    .

--- Assets/02.Scripts/MummyAgent.cs
+++ Assets/02.Scripts/MummyAgent.cs
@@ -13,14 +13,25 @@ public class MummyAgent : Agent
         3. 보상(Reward)
     */
 
+    private Transform tr;
+    private Transform targetTr;
+    private Rigidbody rb;
+
     // 초기화 작업을 하는 메소드
     public override void Initialize()
     {
+        tr = GetComponent<Transform>();
+        targetTr = tr.parent.Find("Target").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // 에피소드(학습의 단위) 시작될때 마다 호출되는 메소드
     public override void OnEpisodeBegin()
     {
+        // 물리력을 초기화
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
     }
 
     // 주변환경을 관측하는 메소드
