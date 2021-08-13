dbb486c16ca79fe403d771413286aeaf743baf19
Lee JaeHyun <myleje@gmail.com>
2021-08-13 17:40:43 +0900

    .

--- Assets/02.Scripts/MummyRay.cs
+++ Assets/02.Scripts/MummyRay.cs
@@ -11,13 +11,28 @@ public class MummyRay : Agent
     private Rigidbody rb;
     private StageManager stageManager;
 
+    public float moveSpeed = 1.5f;
+    public float turnSpeed = 200.0f;
+
     public override void Initialize()
     {
-        MaxStep = 5000;
+        MaxStep = 50;
+
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+        stageManager = tr.parent.GetComponent<StageManager>();
     }
 
     public override void OnEpisodeBegin()
     {
+        // 스테이지를 초기화
+        stageManager.InitStage();
+        // 에이젼트의 위치 변경
+        tr.localPosition = new Vector3(Random.Range(-20.0f, 20.0f),
+                                       0.05f,
+                                       Random.Range(-20.0f, 20.0f));
+
+        tr.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
     }
 
     public override void CollectObservations(VectorSensor sensor)
