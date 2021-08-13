64475f961eb0224829056db1a22ea613772fc570
Lee JaeHyun <myleje@gmail.com>
2021-08-13 17:04:09 +0900

    .

--- Assets/02.Scripts/StageManager.cs
+++ Assets/02.Scripts/StageManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    public GameObject goodItem;
+    public GameObject badItem;
+
+    [Range(10, 50)]
+    public int goodItemCount = 30;
+    [Range(10, 50)]
+    public int badItemCount = 20;
+
     // Start is called before the first frame update
     void Start()
     {
